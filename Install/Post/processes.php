431feab8f44fa781051679af2e278f916ffe625c
Louis Ouellet <louis_ouellet@hotmail.com>
2025-10-15 14:04:37 -0400

    General: Updated the database schematics

---
+++ Install/Post/processes.php
@@ -0,0 +1,18 @@
+<?php
+
+class ProcessesPostModel extends ProcessModel {
+
+    /**
+     * Post process a record
+     *
+     * @param array $record
+     * @return array
+     */
+    public function post($record): array
+    {
+        // Check if the record ID is below 9999
+        if($record['id'] > 9999) return $record;
+
+        return [];
+    }
+}
